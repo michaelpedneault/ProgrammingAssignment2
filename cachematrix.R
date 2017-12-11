5d40e8eb6be4d2e69b37f7c9d3edaf552c58e87b
Michael Pedneault <michaelpedneault@gmail.com>
2017-12-11 15:51:32 -0500

    assigment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates an object called matrix together with 
+## its inverse (set to NULL initially) and four sibbling functions set, get, 
+## setinverse and getinverse. The code is taken from the assigment example and 
+#  modified for matrices.
+# Syntax: myMatrix=makeCacheMatrix(matrix(1:4,2,2))
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) 
+  {
+  x_inv <- NULL
+  set <- function(y) 
+    {
+    x <<- y
+    x_inv <<- NULL
+    }
+  get <- function() x
+  setinverse <- function(inverse) x_inv <<- inverse
+  getinverse <- function() x_inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+  }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the makeCacheMatrix object x.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+  {
+        
+  x_inv <- x$getinverse()
+  if(!is.null(x_inv)) {
+    message("getting cached data")
+    return(x_inv)
+  }
+  data <- x$get()
+  x_inv <- solve(data, ...)
+  x$setinverse(x_inv)
+  x_inv
+  
 }
